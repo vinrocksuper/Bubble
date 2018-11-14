9510a72e129632afe9c634448e46fed2f2f828f9
BT_1N3_29 <vli8558@bths.edu>
2018-11-14 10:17:13 -0500

    timer

--- src/Runner.java
+++ src/Runner.java
@@ -1,21 +1,42 @@
 public class Runner {
     public static void main(String[] args)
     {
-        int[] arr = {1,7,2,4,3,6,5};
+        int[] arr = new int[1000];
+        for(int i=0;i<1000;i++)
+        {
+            arr[i] = (int)(Math.random()*1000);
+        }
         int check = arr.length-1;
-
+        String str ="";
+        for(int i=0;i<arr.length;i++)
+        {
+            str += "[ " + arr[i] + " ]";
+        }
+        System.out.println(str);
+        int sortCount =0;
         boolean swap = true;
+        long time = System.nanoTime();
         while(swap) {
+
             int j = 1;
             swap = false;
             if (check == arr.length - 1) {
                 for (int i = 0; i < arr.length - 1; i++) {
+
                     if (arr[j] < arr[i]) {
                         swap(arr, i, j);
                         swap = true;
                         for(int z = 0; z < arr.length;z++) {
-                            System.out.println(arr[z]);
+                            str ="";
+                            for(int u=0;u<arr.length;u++)
+                            {
+                                str += "[ " + arr[u] + " ]";
+                            }
+
+
+
                         }
+                        sortCount++;
                     }
                     if (arr[j] > arr[i]) {
                         check = j;
@@ -29,9 +50,15 @@ public class Runner {
                         if (arr[j] < arr[i]) {
                             swap(arr, i, j);
                             swap = true;
-                            for(int z = 0; z < arr.length;z++) {
-                                System.out.println(arr[z]);
+                            for(int z = 0; z < arr.length-1;z++) {
+                                str = "";
+                                for(int u=0;u<arr.length;u++)
+                                {
+                                    str += "[ " + arr[u] + " ]";
+                                }
+                                sortCount++;
                             }
+
                         }
                         if (arr[j] > arr[i]) {
                             check = j;
@@ -39,7 +66,11 @@ public class Runner {
                         j++;
                     }
             }
+
         }
+        System.out.println((System.nanoTime() - time)/(1000000000) +  "Seconds");
+
+        System.out.println(str +"  " + sortCount);
     }
     public static void swap(int[] arr, int i, int j)
     {
